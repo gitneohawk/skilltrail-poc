2e4b9474eecf5b1e9cb3dc967eb10a98298bb4be
gitneohawk <neohawk@evoluzio.com>
2025-07-03 17:45:24 +0900

    tailwind.config.js mod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}"
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
